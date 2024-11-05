a04fea35c6f5aa1d0245ae7292f84d3982ab5acd
ramu siripalli <ramsiripalli2772@gmail.com>
2024-11-05 13:43:44 +0530

    app.put ( update api)
    app.put ( update api)

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,6 @@ console.log("Error in fetching products", error.message);
 })
 app.post('/api/products',async(req,res)=>{
     const product = req.body;
-    
     if(!product.name || !product.price || !product.image){
         return res.status(400).json({
             success: false, message:"please provide all the fields"
@@ -45,7 +44,24 @@ app.delete('/api/products/:id',async(req,res) => {
         console.log("Error in create Product: ", error.message);
      res.status(404).json({ success: false, message: "Product not found"});
     }
-} )
+} );
+
+app.put('/api/products/:id', async (req,res) => {
+const { id } = req.params;
+const product = req.body;
+
+try{
+   const updatedProduct =  await Product.findByIdAndUpdate(id,product,{new:true});
+   res.status(200).json({ success : true, data: updatedProduct});
+}catch(error){
+    res.status(505).json({
+        success: false,message:"Server Error"
+    });
+}
+
+});
+
+
 connectDB();
 
 app.listen(3000, ()=> {

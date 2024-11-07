9873dfadd70b7a4b994162ccd47e881c55ea2e4f
ramu siripalli <ramsiripalli2772@gmail.com>
2024-11-07 05:59:31 +0530

    added PORT into ENV file

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,16 @@ import express from 'express';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import productRoutes from './routes/product.route.js';
-
 dotenv.config();
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
 
 
 
-app.listen(3000, ()=> {
+app.listen(PORT, ()=> {
     connectDB();
-    console.log("server started at port:3000 ");
+    console.log("server started at https://localhost:" + PORT);
 })
